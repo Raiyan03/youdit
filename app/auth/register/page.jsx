2db43fdcdbcb14f577a0277b821c276437c768cd
Ryan <ryansheikh003@gmail.com>
2024-07-10 16:19:09 -0600

    fixed(ui): Fixing the overlapping of the slidable components
    The sliding components in the authentication forms are now not overlapping with the background while rendering

--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -1,8 +1,10 @@
 import EditorForm from "@/components/auth/register/form";
-import RegisterForm from "@/components/auth/register/register";
+import YoutuberForm from "@/components/auth/register/youtuber-form";
+// import RegisterForm from "@/components/auth/register/register";
+import AuthForm from "@/components/auth/auth-form";
 const Register = () => {
     return (
-        <RegisterForm />
+        <AuthForm EditorForm={EditorForm} YoutuberForm={YoutuberForm}  />
     );
 }
 
--- components/auth/auth-form.jsx
+++ components/auth/auth-form.jsx
@@ -1,11 +1,11 @@
 "use client"
 import { useState } from "react";
-import EditorForm from "./form";
-import YoutuberForm from "./youtuber-form";
-const RegisterForm = () => {
+// import EditorForm from "./form";
+// import YoutuberForm from "./youtuber-form";
+const AuthForm = ({YoutuberForm, EditorForm}) => {
     const [isYoutuber, setYoutuber] = useState(false);
     return (
-        <div className="flex flex-col absolute top-10 mx-auto shadow-lg rounded-lg max-w-md bg-white">
+        <div className="flex flex-col overflow-hidden absolute top-10 mx-auto shadow-lg rounded-lg max-w-md bg-white">
             <div className="flex">
                 <button onClick={() => setYoutuber(false)} className={`w-1/2 py-2 ${!isYoutuber ? "bg-red-600 text-white" : "text-red-600"} rounded-tl-lg`}>
                     Editor
@@ -25,4 +25,4 @@ const RegisterForm = () => {
     )
 };
 
-export default RegisterForm;
+export default AuthForm;

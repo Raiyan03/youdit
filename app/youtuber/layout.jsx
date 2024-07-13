78ed6c0bf3341951e0c4ab12358cedb1c265cf36
Ryan <ryansheikh003@gmail.com>
2024-07-13 13:23:13 -0600

    feat(ui) : Add side bar
    Added side with fixed styling has route buttons and logout button

--- app/youtuber/layout.jsx
+++ app/youtuber/layout.jsx
@@ -3,14 +3,19 @@ import Sidebar from "@/components/youtuber/sidebar";
 const YoutubeLayout = ({children}) => {
     return (
         <div className="flex flex-col min-h-screen bg-background">
-            <Navbar />
-            <div className="flex flex-grow">
-                <Sidebar/>
-                <main className="flex-1 p-3">
+            <div className="fixed w-full z-10">
+                <Navbar />
+            </div>
+            <div className="flex flex-grow pt-16 relative"> {/* Adjust padding-top to the height of your Navbar if needed */}
+                <div className="fixed z-30">
+                    <Sidebar />
+                </div>
+                <main className=" z-0 ml-[200px] flex-1 p-3 h-full">
                     {children}
                 </main>
             </div>
         </div>
+
     );
 }
 
---
+++ components/youtuber/navlinks.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function NavLinks({ item }) {
+  return (
+    <Link href={item.path}
+      className="flex 
+      flex-row 
+      items-center 
+      gap-4
+      text-lg 
+      px-2 
+      py-1 
+      text-neutral-700 
+      transition 
+      duration-150 
+      ease-in-out 
+      hover:border-b-2
+      hover:border-accent1 
+      hover:shadow-lg 
+      active:shadow-inner 
+      active:bg-red-300
+      hover:rounded-md 
+      hover:text-black">
+      {item.icon}
+      {item.title}
+    </Link>
+  )
+}

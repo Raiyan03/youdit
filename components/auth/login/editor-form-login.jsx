c99407c2ab2bc3f4023e4b4c5b66431bb5bffb7c
Ryan <ryansheikh003@gmail.com>
2024-07-13 00:07:28 -0600

    feat(input validation): Validate inputs on the login page
    Implemented a validation fonction in
    
    Displaying proper error messages
    
    Checking if email format is correct with regex
    
    Checking if fields are empty

--- components/auth/login/editor-form-login.jsx
+++ components/auth/login/editor-form-login.jsx
@@ -1,11 +1,11 @@
 "use client"
+import { loginInputValidation } from "@/lib/validation";
 import { useEffect, useState } from "react";
+import ErrorBox from "../error";
 const EditorFormLogin = () => {
     const [isLoaded, setIsLoaded] = useState(false);
-    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
     const [errorMessage, setError] = useState("");
 
     const handleChangName = (e) => {
@@ -26,31 +26,14 @@ const EditorFormLogin = () => {
         setIsLoaded(true);
     }, []);
 
-    const Register = (e) => {
+    const Login = (e) => {
         e.preventDefault();
-        const response = registrationInputValidation({name, email, password, confirmPassword});
-        if (response.error) {
-            setError(response.error);
-            return ;
-        }
-        const values = {
-            name: name,
-            email: email,
-            password: password
-        }
-        try{
-            axios.post("/api/auth/register", values)
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((err) => {
-                setError(err.response.data.message);
-            })
-        }
-        catch(e){
-            setError(e.message);
-        }
         setError("");
+        const response = loginInputValidation({ email, password });
+        if (response?.error) {
+            setError(response.error);
+            return;
+        }
     }
 
     return (
@@ -58,19 +41,13 @@ const EditorFormLogin = () => {
             <h1 className="text-2xl font-semibold text-gray-800 mb-6">
                 Welcome to YouDit! 🎥
             </h1>
-            <form onSubmit={Register} className={`w-full flex flex-col gap-4 transition-all duration-1000 ease-out ${isLoaded ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0'}`}>
-                <input onChange={handleChangName} className="border border-gray-300 focus:ring-2 focus:ring-red-500 focus:outline-none rounded-lg p-3" 
-                       type="text" 
-                       placeholder="Enter your name" />
+            <form onSubmit={Login} className={`w-full flex flex-col gap-4 transition-all duration-1000 ease-out ${isLoaded ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0'}`}>
                 <input onChange={handleChangeEmail} className="border border-gray-300 focus:ring-2 focus:ring-red-500 focus:outline-none rounded-lg p-3" 
                        type="email" 
                        placeholder="Enter your email" />
                 <input onChange={handleChangePassword} className="border border-gray-300 focus:ring-2 focus:ring-red-500 focus:outline-none rounded-lg p-3" 
                        type="password" 
                        placeholder="Create a password" />
-                <input onChange={handleChangeConfirmPassword} className="border border-gray-300 focus:ring-2 focus:ring-red-500 focus:outline-none rounded-lg p-3" 
-                       type="password" 
-                       placeholder="Confirm your password" />
                 {errorMessage ? <ErrorBox message={errorMessage}  /> : null}
                 <button type="submit" className="py-3 px-5 bg-primary text-white rounded-lg font-medium hover:bg-accent transition duration-300 ease-in-out"> 
                     Join Now

0d41c50e7f4fa870ea0ee80ed1aeb71d6ac760bc
Ryan <ryansheikh003@gmail.com>
2024-07-13 12:04:23 -0600

    feat(ui): Add Navbar
    Added a navbar ui component with app logo and Account picture

--- app/youtuber/layout.jsx
+++ app/youtuber/layout.jsx
@@ -11,7 +11,6 @@ const YoutubeLayout = ({children}) => {
                 </main>
             </div>
         </div>
-
     );
 }
 
--- components/youtuber/navbar.jsx
+++ components/youtuber/navbar.jsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+
 const Navbar = () => {
     return (
-        <div>
-            This is a navbar
+        <div className=" h-[60px] flex items-center justify-between bg-background px-3 border-b shadow-md">
+            <div className="flex items-center">
+                <Image src="/logo2.png" height={80} width={80} />
+                <h1 className=" text-xl font-bold">
+                    YouDit
+                </h1>
+            </div>
+            <Image 
+            src="https://lh3.googleusercontent.com/a/ACg8ocJuR7dMRe2CpFYa3N3XYwExQwcxWWDWhqF0ax2CZZ6e_scrvQ=s96-c"
+            height={33}
+            width={33}
+             className="rounded-full"
+            />
         </div>
     )
 }

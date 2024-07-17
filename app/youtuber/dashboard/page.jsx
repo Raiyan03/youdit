0c9a84d623b388b7d5bbc2c9bf99f4e6e9241810
Ryan <ryansheikh003@gmail.com>
2024-07-16 20:32:45 -0600

    feat (ui): Temporary Dashboard page
    Created a dashboard page with some cards having some directional steps to use the application

--- app/youtuber/dashboard/page.jsx
+++ app/youtuber/dashboard/page.jsx
@@ -1,11 +1,15 @@
 import UserBatch from "@/components/youtuber/user-batch";
 import { auth } from "@/auth";
-
+import Card from "@/components/youtuber/dashboard/card";
 const YoutuberDashboard = async () => {
     const session = await auth();
     return (
-        <div className="flex items-center flex-col justify-center">
+        <div className="flex items-center gap-10 flex-col justify-center">
             <UserBatch user={session?.user} />
+            <div className="flex flex-col justify-between w-full gap-7  sm:flex-col md:flex-row items-center">
+                <Card title="Add an Editor!" description="Don't have an editor assigned, add one now!" image="/editor.png" link="/youtuber/videos" />
+                <Card title="No videos in production" description="Assign a video to your editors and have a status on your videos" image="/editor.png" link="/youtuber/videos" />
+            </div>
         </div>
     );
 }
--- components/youtuber/dashboard/card.jsx
+++ components/youtuber/dashboard/card.jsx
@@ -4,16 +4,16 @@ import { GrLinkNext } from "react-icons/gr";
 
 const Card  = ({ title, description, image, link}) => {
     return (
-        <div className="flex flex-col w-1/5 p-3 gap-3 justify-center items-center border rounded-lg">
+        <div className="flex flex-col w-1/2 p-3 gap-3 justify-center items-center border rounded-lg">
             <h1 className="text-lg font-semibold self-start">
                 {title}
             </h1>
-            <Image src={image} width={70} height={70} />
+            <Image src={image} width={100} height={100} />
             <p className="w-full text-wrap">
                 {description}
             </p>
             <Link className=" self-end" href={link} >
-                <GrLinkNext />
+                <GrLinkNext className="hover:text-primary transition duration-400 ease-in-out" />
             </Link>           
         </div>
     )

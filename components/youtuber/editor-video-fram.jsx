e8d0e97365299928999c1fe5d4e949ad758e69b8
Ryan <ryansheikh003@gmail.com>
2024-07-19 16:04:15 -0600

    feat (ui) : Add dropdown menu for editors icon
    Added dropdown mwnu for each editors int on the dashboard page
    
    Implemented animation for sliding down and sliding up

--- components/youtuber/editor-video-fram.jsx
+++ components/youtuber/editor-video-fram.jsx
@@ -6,6 +6,7 @@ import { FaChevronDown } from "react-icons/fa";
 const Frame = ({ data }) => {
     const [visible, setVisible] = useState(false);
     const [rotated, setRotated] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const { link, title } = data;
 
     const hover = () => {
@@ -18,17 +19,26 @@ const Frame = ({ data }) => {
 
     const handleClick = () => {
         setRotated(!rotated);
+        setIsOpen(!isOpen);
     };
 
     return (
-        <div onMouseEnter={hover} onMouseLeave={leaveHover} onClick={handleClick} className="flex justify-between cursor-pointer p-1 rounded-sm items-center w-full bg-gray-200">
-            <div className="flex gap-2 items-center">
-                <Image src={link} width={50} height={50} />
-                <h1>{title}</h1>
+        <div onMouseEnter={hover} onMouseLeave={leaveHover} onClick={handleClick} className="flex flex-col cursor-pointer p-1 rounded-sm items-center w-full bg-gray-200">
+            <div className="flex justify-between w-full items-center">
+                <div className="flex gap-2 items-center">
+                    <Image src={link} width={50} height={50} />
+                    <h1>{title}</h1>
+                </div>
+                {visible && (
+                    <FaChevronDown className={`transform transition-transform duration-300 ${rotated ? 'rotate-180' : 'rotate-0'}`} />
+                )}
             </div>
-            {visible && (
-                <FaChevronDown className={`transform transition-transform duration-300 ${rotated ? 'rotate-180' : 'rotate-0'}`} />
-            )}
+            <div className={`transition-all duration-300 w-full ease-in-out overflow-hidden ${isOpen ? 'max-h-40 opacity-100 focus-within:' : 'max-h-0 opacity-0'}`}>
+                <div className="flex border-red-800 w-full flex-col gap-2 p-2">
+                    <button className="p-2 bg-gray-200 rounded-sm">Edit</button>
+                    <button className="p-2 bg-gray-200 rounded-sm">Delete</button>
+                </div>
+            </div>
         </div>
     );
 }
